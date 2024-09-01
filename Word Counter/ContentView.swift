697c634179c01bf4e9fe9c7f0cc3f5edc4891067
armaan535kh <kh.armaan535@gmail.com>
2024-08-31 22:01:29 -0700

    Word counting application

--- Word Counter/ContentView.swift
+++ Word Counter/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var words = ""
+    
+    var wordCount: Int {
+        return words.split(separator: " ").count
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Spacer()
+            Text("Type in the text Box and click on the buttton to count the words")
+            Spacer()
+            Spacer()
+            
+            TextEditor(text: $words)
+                .frame(minHeight: 100)
+                .border(Color.gray)
+           
+            
+            Text("Word count : \(wordCount)")
+            
+            Spacer()
+            Spacer()
+            Spacer()
+            
+            
+            
+            
+                .navigationTitle("Word Counter")
+                .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        
+        
     }
 }
 
