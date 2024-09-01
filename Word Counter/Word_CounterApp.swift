9269623392d854e0b3e780eb86de3f452e0ff908
armaan535kh <kh.armaan535@gmail.com>
2024-08-31 20:25:09 -0700

    Initial Commit

---
+++ Word Counter/Word_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_CounterApp.swift
+//  Word Counter
+//
+//  Created by Armaan Khan  on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
